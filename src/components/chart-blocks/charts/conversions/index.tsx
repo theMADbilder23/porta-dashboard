8ad885098f08c0c72d3cfe9a45520d9effca1185
theMADbilder23 <madmoneyinvestments23@gmail.com>
2026-03-22 17:01:16 -0400

    change conversions title icon

--- src/components/chart-blocks/charts/conversions/index.tsx
+++ src/components/chart-blocks/charts/conversions/index.tsx
@@ -1,4 +1,4 @@
-import { CirclePercent } from "lucide-react";
+import { Layers3 } from "lucide-react";
 import conversions from "@/data/conversions";
 import ChartTitle from "../../components/chart-title";
 import Chart from "./chart";
@@ -6,7 +6,7 @@ import Chart from "./chart";
 export default function Conversions() {
   return (
     <section className="flex h-full flex-col gap-2">
-      <ChartTitle title="MMII Allocation" icon={CirclePercent} />
+      <ChartTitle title="MMII Allocation" icon={Layers3} />
       <div className="relative max-h-80 flex-grow">
         <Chart />
       </div>
