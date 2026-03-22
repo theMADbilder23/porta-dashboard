e2d2ba54f915f0667953178429da6c03978c8cd1
theMADbilder23 <madmoneyinvestments23@gmail.com>
2026-03-22 16:46:26 -0400

    modifying conversions section wording

--- src/components/chart-blocks/charts/conversions/index.tsx
+++ src/components/chart-blocks/charts/conversions/index.tsx
@@ -1,6 +1,5 @@
 import { CirclePercent } from "lucide-react";
 import conversions from "@/data/conversions";
-import { addThousandsSeparator } from "@/lib/utils";
 import ChartTitle from "../../components/chart-title";
 import Chart from "./chart";
 
@@ -8,23 +7,9 @@ export default function Conversions() {
   return (
     <section className="flex h-full flex-col gap-2">
       <ChartTitle title="MMII Allocation" icon={CirclePercent} />
-      <Indicator />
       <div className="relative max-h-80 flex-grow">
         <Chart />
       </div>
     </section>
-  );
-}
-
-function Indicator() {
-  return (
-    <div className="mt-3">
-      <span className="mr-1 text-2xl font-medium">
-        {addThousandsSeparator(
-          conversions.reduce((acc, curr) => acc + curr.value, 0),
-        )}
-      </span>
-      <span className="text-muted-foreground/60">Portolio Allocation</span>
-    </div>
   );
 }
