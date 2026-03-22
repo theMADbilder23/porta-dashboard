2324f43b3bb2cd5b4df45bffc85e4bd696a4400c
theMADbilder23 <madmoneyinvestments23@gmail.com>
2026-03-22 17:49:23 -0400

    conversions bubbles enhancements

--- src/components/chart-blocks/charts/conversions/chart.tsx
+++ src/components/chart-blocks/charts/conversions/chart.tsx
@@ -5,11 +5,18 @@ import type { ICirclePackingChartSpec } from "@visactor/vchart";
 import conversions from "@/data/conversions";
 import { addThousandsSeparator } from "@/lib/utils";
 
+const totalAllocation = conversions.reduce((sum, item) => sum + item.value, 0);
+
+const conversionData = conversions.map((item) => ({
+  ...item,
+  percentage: Math.round((item.value / totalAllocation) * 100),
+}));
+
 const spec: ICirclePackingChartSpec = {
   data: [
     {
       id: "data",
-      values: conversions,
+      values: conversionsData,
     },
   ],
   type: "circlePacking",
@@ -23,7 +30,7 @@ const spec: ICirclePackingChartSpec = {
       fill: "white",
       stroke: false,
       visible: (d) => d.depth === 0,
-      text: (d) => `${Math.round((d.value / conversions.reduce((acc, curr) => acc + curr.value, 0)) * 100)}%`,
+      text: (d) => `${d.percentage}%`,
       fontSize: (d) => d.radius / 2,
       dy: (d) => d.radius / 8,
     },
