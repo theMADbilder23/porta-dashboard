fa80b4836893dafc241799954768a5c4f7b23c0d
theMADbilder23 <madmoneyinvestments23@gmail.com>
2026-03-21 23:56:22 -0400

    update chart backgrounds

--- src/config/chart-theme.ts
+++ src/config/chart-theme.ts
@@ -2,8 +2,10 @@ import type { ITheme } from "@visactor/vchart";
 
 export const customDarkTheme: Partial<ITheme> = {
   type: "dark",
-  background: "#020817",
+  background: "#050508",
 };
+
 export const customLightTheme: Partial<ITheme> = {
   type: "light",
+  background: "#F7F2FF",
 };
